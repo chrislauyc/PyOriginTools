7688f707b51d56f01abb7828385fe42c978f3d7a
Scott W Harden <swharden@gmail.com>
2016-11-30 13:22:25 -0500

    PyPi build 0.1.031

--- scripts/BUILD AND UPLOAD.bat
+++ scripts/BUILD AND UPLOAD.bat
@@ -13,7 +13,7 @@ echo.
 echo    You are are about to UPLOAD version %version% to PyPi 
 echo.
 pause
-python setup.py sdist
+python setup.py sdist upload
 
 :: using "git" from the command line may require that you add the path to git.exe to your system path:
 :: C:\Users\swharden\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\cmd\
