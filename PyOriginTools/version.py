69d53d915500c73607b67a7bd96960ab21d6b013
Scott W Harden <swharden@gmail.com>
2016-12-01 11:47:31 -0500

    PyPi build 0.1.052

--- PyOriginTools/version.py
+++ PyOriginTools/version.py
@@ -1,3 +1,3 @@
-__counter__=51
+__counter__=52
 __release__=''
 __version__='0.1.%03d'%__counter__+__release__
--- scripts/BUILD AND UPLOAD.bat
+++ scripts/BUILD AND UPLOAD.bat
@@ -1,89 +1,15 @@
-:: this script MSUT be run from inside this folder
 @echo OFF
-
 set /p moduleName=<moduleName.txt
-
-
-echo.
-echo   ################################
-echo   ### UPDATING VERSION COUNTER ###
-echo   ################################
-echo.
-Timeout /t 3 /nobreak
-
-:: update version.py with the new version and create version.bat
 python update_version.py
-:: pull the version in from the text file that was generated
 set /p version=<version.txt
-:: from here on we must be in the root folder
 cd ..
-
-
-
-
-
-
-
-echo.
-echo   #########################
 echo   ### UPLOADING TO PYPI ###
-echo   #########################
-echo.
-Timeout /t 3 /nobreak
 python setup.py sdist upload
-
-
-
-
-
-
-echo.
-echo   ###############################
-echo   ### UPGRADING LOCAL PACKAGE ###
-echo   ###############################
-echo.
-Timeout /t 3 /nobreak
-pip install --upgrade --no-cache-dir %moduleName%
-
-
-
-
-
-
-
-::echo.
-::echo   ###################################
-::echo   ### CREATING AND UPLOADING DOCS ###
-::echo   ###################################
-::echo.
-::Timeout /t 3 /nobreak
-::python setup.py sdist upload_docs
-
-
-
-
-
-echo.
-echo   #################################
 echo   ### COMMITTING CHANGES TO GIT ###
-echo   #################################
-echo.
-Timeout /t 3 /nobreak
-
 :: using "git" from the command line may require that you add the path to git.exe to your system path:
 :: C:\Users\swharden\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\cmd\
-
-:: collect all changes made during the build
 git add --all
-:: now commit those changes
 git commit -m "PyPi build %version%"
-
-
-
-echo.
-echo.
-echo.
-echo DONE! Remember that a GitHub Sync is required. Launching desktop client...
-echo.
-"%appdata%\..\Local\GitHub\GitHub.appref-ms"
+echo ### DONE! ###
+echo Remember that a GitHub Sync is required.
 pause
