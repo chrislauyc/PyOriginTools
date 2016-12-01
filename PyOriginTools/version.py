bbe1ec98f15f72a02e34f016bc0e280bedaf5650
Scott W Harden <swharden@gmail.com>
2016-12-01 08:34:50 -0500

    PyPi build 0.1.040

--- PyOriginTools/version.py
+++ PyOriginTools/version.py
@@ -1,3 +1,3 @@
-__counter__=39
+__counter__=40
 __release__=''
 __version__='0.1.%03d'%__counter__+__release__
--- scripts/BUILD AND UPLOAD.bat
+++ scripts/BUILD AND UPLOAD.bat
@@ -14,7 +14,7 @@ echo    You are are about to UPLOAD version %version% to PyPi
 echo.
 pause
 python setup.py sdist upload 
-python setup.py sdist upload_docs --upload-dir=docs/pythonhosted
+python setup.py upload_docs --upload-dir=docs/pythonhosted
 
 :: using "git" from the command line may require that you add the path to git.exe to your system path:
 :: C:\Users\swharden\AppData\Local\GitHub\PortableGit_d7effa1a4a322478cd29c826b52a0c118ad3db11\cmd\
