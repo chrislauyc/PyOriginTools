1706e4d554f321801f92966bb2c6e84aff5e3351
Scott W Harden <swharden@gmail.com>
2016-12-09 19:02:50 -0500

    PyPi build 0.1.056

---
+++ documentation/demonstrations/phasic.py
@@ -0,0 +1,13 @@
+r"""
+run -pyf C:\Users\swharden\Documents\GitHub\PyOriginTools\documentation\demonstrations\basics.py
+"""
+
+import sys
+if False:
+    # this code block will NEVER actually run
+    sys.path.append('../../') # helps my IDE autocomplete
+
+import PyOriginTools as OR
+
+if __name__=="__main__":
+    abfPath=R"X:\Data\2P01\2016\2016-09-01 PIR TGOT\16d07022.abf"
